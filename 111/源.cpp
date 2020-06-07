20d9638bbf0789f653a3af2732c79635fe1dc0ae
hequhecong-0 <780077191@qq.com>
2020-06-07 19:30:51 +0800

    添加项目文件。

---
+++ 111/源.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class A
+{
+public:
+	int a;
+	double b;
+};
+int main()
+{
+	string A("hello ");
+	string B("world");
+	string C;
+	C = A + B;
+	return 0;
+}
